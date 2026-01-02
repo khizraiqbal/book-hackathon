ed25e4b613250d718e015d5fa1bd891e2573de4f
khizraiqbal <160323713+khizraiqbal@users.noreply.github.com>
2026-01-03 02:32:59 +0500

    Update production URL in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   // },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://book-hackathon-33ii-qq0t2gnf6-khizra-iqbals-projects-2ac55d15.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
